32ff1043f6da503308fc89e1f80e26abd3fbc2b2
Mohapatra <AM1742@zebra.com>
2022-01-12 16:33:47 +0530

    Event binding, 2way binding, dynamiclist, conditionalmsg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-app';
+  name = 'Ashwini Mohapatra';
+
+  selectedlang: string ='';
+  listoflang: string[]=["English","Hindi","French","Turkish"];
+  
+  addLang(){
+    console.log("Button Pressed");
+    this.listoflang.push(this.selectedlang);
+    console.log(this.listoflang);
+  }
+  deletelang(){
+    this.listoflang.splice(this.listoflang.length-1,this.listoflang.length);
+  }
 }
